478ed971fc91b79f86c8440364101f8d547aa0f3
sanskar11 <sanskartibrewal11@gmail.com>
2019-11-17 15:50:10 +0530

    run.sh corrected

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
 shell=$SHELL
-if [ shell == "/bin/bash" ]
+echo $shell
+if [ $shell == "/bin/bash" ]
 then
-	echo "alias cd=. ~/cd/cd" >>~/.bashrc
+	echo "alias cd='. ~/cd/cd'" >>~/.bashrc
 else
 	echo This script is only used for bash
 fi
